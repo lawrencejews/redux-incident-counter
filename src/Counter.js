2e22aed8015487cc9074944ab709044774e82268
lawrencejews <lawrencejews1@gmail.com>
2021-07-02 12:26:58 +0900

    first commit

--- src/Counter.js
+++ src/Counter.js
@@ -1,16 +1,19 @@
+import { useCounter } from "./use-counter";
+
 export const Counter = () => {
   const incident = 'Incident';
-  const count = 0;
+  const { count, increment, decrement, set } = useCounter();
 
   return (
     <main className="Counter">
       <h1>Days Since Last {incident}</h1>
       <p className="count">{count}</p>
       <section className="controls">
-        <button>Increment</button>
-        <button>Reset</button>
-        <button>Decrement</button>
+        <button onClick={() => decrement()}>Decrement</button>
+        <button onClick={() => set(0)}>Reset</button>
+        <button onClick={() => increment()}>Increment</button>
       </section>
+      <setCounter/>
     </main>
   );
 };
